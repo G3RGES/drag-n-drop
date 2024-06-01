e05122c76766c513760310a30d9d4f865a58909c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-01 13:28:21 +0300

    Update Bin.jsx

--- src/components/Bin.jsx
+++ src/components/Bin.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { useDrop } from "react-dnd";
+
 const Bin = () => {
-  return <div>Bin</div>;
+  const [{ canDrop, isOver }, drop] = useDrop(
+    () => ({
+      accept: "note",
+      drop: () => ({ name: "bin" }),
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+        canDrop: !!monitor.canDrop(),
+      }),
+    }),
+    []
+  );
+
+  return (
+    <div className="border h-48 " ref={drop}>
+      <h1 className="text-center font-medium py-2 ">Bin</h1>
+    </div>
+  );
 };
 
 export default Bin;
