8c205159e584f9554235d2b048e0d552e75f0786
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-01 13:10:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import Note from "./components/Note";
 import Bin from "./components/Bin";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [binnedItems, setBinnedItems] = useState([]);
+
+  useEffect(() => {
+    const initialNotes = ["Note 1", "Note 2", "Note 3"];
+    localStorage.setItem("notesList", JSON.stringify(initialNotes));
+
+    let array = localStorage.getItem("notesList");
+    setNotes(JSON.parse(array));
+  }, []);
+
+  // console.log(notes); //* TESTING
+
   return (
     <DndProvider backend={HTML5Backend}>
       <h1 className="text-3xl text-center font-semibold mt-4 py-2 ">
         React DnD
       </h1>
 
-      <Note />
+      {notes.map((note) => (
+        <Note
+          key={new Date().getTime() + Math.floor(Math.random() * 1000)}
+          note={note}
+        />
+      ))}
       <Bin />
     </DndProvider>
   );
