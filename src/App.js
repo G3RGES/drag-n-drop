1035b602c2d88873b50c77466ad3b0e49b4041b7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-01 12:53:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 function App() {
   return (
-    <>
-      <h1 className="text-xl">React DnD</h1>
-    </>
+    <DndProvider backend={HTML5Backend}>
+      <h1 className="text-3xl text-center font-semibold mt-4 py-2 ">
+        React DnD
+      </h1>
+    </DndProvider>
   );
 }
 
