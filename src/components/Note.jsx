33515ba2824ec6713972c43b3c0f62e02178a8fc
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-01 13:40:34 +0300

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { useDrag } from "react-dnd";
 
-const Note = ({ note }) => {
+const Note = ({ note, binnedItems }) => {
   const [{ isDragging }, drag] = useDrag(
     () => ({
       type: "note",
@@ -11,6 +11,10 @@ const Note = ({ note }) => {
         const dropResult = monitor.getDropResult();
         if (item && dropResult) {
           alert(`You threw ${item.name} Into ${dropResult.name}`);
+          let tempList = binnedItems;
+          tempList.push(item.name);
+          localStorage.setItem("binnedItems", JSON.stringify(tempList));
+          window.location.reload();
         }
       },
       collect: (monitor) => ({
